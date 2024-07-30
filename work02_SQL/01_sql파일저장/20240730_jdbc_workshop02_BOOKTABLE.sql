71fef06accb62887f0fcbeaec7c8c02344de81b3
HJLee <heejulia21@naver.com>
2024-07-30 17:36:25 +0900

    20240730_add_modified file

--- work02_SQL/01_sql파일저장/20240730_jdbc_workshop02_BOOKTABLE.sql
+++ work02_SQL/01_sql파일저장/20240730_jdbc_workshop02_BOOKTABLE.sql
@@ -8,6 +8,14 @@ publisher varchar(15) not null,
 price int not null,
 description varchar(200));
 
-DESC book;
+SELECT * FROM book;
+
+SELECT isbn, title, author, publisher, price, description FROM book WHERE title like '%java%';
+
+SELECT isbn, title, author, publisher, price, description FROM book WHERE title like '%java%';
+
+SELECT isbn, title, author, publisher, price, description FROM book WHERE title like '% %' OR publisher like '% %' OR author like '%자앤%';
+
+DESC book;SELECT isbn, title, author, publisher, price*0.8, description FROM book WHERE publisher like "%자앤%";
 
 ALTER TABLE BOOK ADD CONSTRAINT BOOK_isbn_PK PRIMARY KEY(isbn);
