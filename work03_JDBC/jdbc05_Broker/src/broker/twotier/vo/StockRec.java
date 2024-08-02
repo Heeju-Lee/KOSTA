cd78c5313862d100b643ee0a4bd9fc2ddcbfa9ff
HJLee <heejulia21@naver.com>
2024-08-02 17:25:10 +0900

    2024.08.02_JDBC Final

---
+++ work03_JDBC/jdbc05_Broker/src/broker/twotier/vo/StockRec.java
@@ -0,0 +1,34 @@
+package broker.twotier.vo;
+
+//주식정보를 저장하는 vo클래스.....
+
+public class StockRec {
+	
+	private String symbol;
+	private float price;
+	
+	public StockRec() {}
+	public StockRec(String symbol, float price) {
+		super();
+		this.symbol = symbol;
+		this.price = price;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "StockRec [symbol=" + symbol + ", price=" + price + "]";
+	}
+
+}
